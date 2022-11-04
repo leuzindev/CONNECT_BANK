2097df41d5aa086c56314ca78d91f7481ffad277
Leonardo_Soares <leonardoc.soares08@gmail.com>
2022-11-03 23:59:59 -0300

    sua conta tela

--- src/Components/Container_Conta/Container_Conta.jsx
+++ src/Components/Container_Conta/Container_Conta.jsx
@@ -8,6 +8,16 @@ const Card_Esquerda = styled.div`
     width: 40%;
     border-top-left-radius: 10px;
     border-bottom-left-radius: 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    @media(max-width: 750px){  
+        width: 100%;
+        background-color: rgba(65, 211, 189, 0.541);
+        border-radius: 10px;
+        
+  }
 `
 const Card_Direita = styled.div`
     background-color: #FFFFFF;
@@ -15,6 +25,11 @@ const Card_Direita = styled.div`
     width: 40%;
     border-top-right-radius: 10px;
     border-bottom-right-radius: 10px;
+    @media(max-width: 750px){  
+        display: none;
+        
+  }
+  
 `
 
 const Blocos = styled.div`
@@ -22,13 +37,19 @@ const Blocos = styled.div`
     justify-content: space-around;
     align-items: center;
     height: 300px;
+    @media(max-width: 1200px){
+    flex-direction: column;
+    height: 60%;
+  }
+    
+ 
 `
 const Bloco_direito = styled.div`
     
 
 `
 const Bloco_esquerdo = styled.div`
-    
+
 
 `
 
@@ -42,10 +63,17 @@ const Area_apagar = styled.div`
     display: flex;
     align-items: flex-end;
     height: 500px;
+ 
+    @media(max-width: 1200px){  
+        height: 300px;
+        
+  }
 `
 
 const Btn_apagar = styled.button`
-    margin: 35px;
+    margin-left: 10%;
+    margin-bottom: 40px;
+    
     background-color: #FF0000;
     color: white;
     cursor: pointer;
@@ -55,24 +83,98 @@ const Btn_apagar = styled.button`
     height: 55px;
     width: 220px;
     font-size: 18pt;
+    @media(max-width: 1200px){  
+        margin-left: 20%;
+        
+  }
 `
+
+const Input_Conta = styled.div`
+    background-color: #FFFFFF;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    border-radius: 10px;
+    width: 85%;
+    height: 60px;
+    margin: 20px;
+
+
+`
+
+const Conteudo_input = styled.p`
+    margin-left: 30px;
+    color: #808080;
+    font-weight: 600;
+`
+
+const Bloco_title = styled.div`
+   
+    width: 85%;
+    height: 100px;
+`
+
+const Titulos = styled.h1`
+    font-size: 40px;
+    color: white;
+    font-family: 'Inter', sans-serif;
+    font-weight: 400;
+
+`
+
+const Saldo = styled.p`
+    font-family: 'Inter', sans-serif;
+    font-size: 20px;
+    color: #808080;
+    font-weight: bold;
+    display: flex;
+`
+
+const Info = styled.p`
+    font-family: 'Inter', sans-serif;
+    font-size: 20px;
+    color: #808080;
+    font-weight: bold;
+`
+const Verde = styled.p`
+    color: #41D3BE;
+    margin-right: 4px;
+`
+
 const Container_Conta = () => {
 
 
   return (
   <>
     <Card_Esquerda> 
-        
+        <Bloco_title>
+            <Titulos>Conta de Fulano</Titulos>
+        </Bloco_title>
+        <Input_Conta>
+            <Conteudo_input>Leonardo Cunha Soares</Conteudo_input>
+        </Input_Conta>
+        <Input_Conta>
+            <Conteudo_input>Leonardo@leonardo.com</Conteudo_input>
+        </Input_Conta>
+        <Input_Conta>
+            <Conteudo_input>(11) 9926-2123</Conteudo_input>
+        </Input_Conta>
+        <Input_Conta>
+            <Conteudo_input>526.551.551.115-44</Conteudo_input>
+        </Input_Conta>
+        <Input_Conta>
+            <Conteudo_input>12351-121321-5212-21</Conteudo_input>
+        </Input_Conta>
     </Card_Esquerda>
     <Card_Direita> 
         <Blocos>
             <Bloco_esquerdo>
-                <p>Total de saldo na conta</p>
-                <p>R$ 0,00</p>
+                <Info>Total de saldo na conta</Info>
+                <Saldo><Verde>R$</Verde>  0,00</Saldo>
                 <br />
                 <br />
-                <p>Total fatura da conta</p>
-                <p>R$ 0,00</p>
+                <Info>Total fatura da conta</Info>
+                <Saldo><Verde>R$</Verde>  0,00</Saldo>
             </Bloco_esquerdo>
             <Bloco_direito>
                 <User_img src={user}/>
